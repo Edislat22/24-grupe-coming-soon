b6067ebc272f9fd3a42390b7739b510d6ec5499e
Rimantas <belauzas@yahoo.com>
2021-02-09 15:43:06 +0200

    Static methods for validation

--- js/components/forms/Forms.js
+++ js/components/forms/Forms.js
@@ -1,3 +1,6 @@
+import { Validation } from './Validation.js';
+
+
 class Forms {
     constructor() {
         this.forms = [];
@@ -23,11 +26,27 @@ class Forms {
 
             const submit = form.querySelector('.btn');
 
-            submit.addEventListener('click', (event) => {
+            submit.addEventListener('click', event => {
                 event.preventDefault();
 
                 for (const input of allInputs) {
-                    console.log(input.value);
+                    const rule = input.dataset.validationRule;
+                    const text = input.value;
+
+                    switch (rule) {
+                        case 'name':
+                            console.log(Validation.isValidName(text));
+                            break;
+                        case 'email':
+                            console.log(Validation.isValidEmail(text));
+                            break;
+                        case 'text':
+                            console.log(Validation.isValidText(text));
+                            break;
+
+                        default:
+                            break;
+                    }
                 }
             })
         }
